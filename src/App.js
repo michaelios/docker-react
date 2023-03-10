6dd5c2a4d497b181f433e755d2771117083e60c8
Misha Ioseliani <misha.ioseliani.7@gmail.com>
2023-03-10 15:50:25 +0400

    add dockerfile for production environment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi There! Hello Michael!
+          You are aswesome!
         </p>
         <a
           className="App-link"
